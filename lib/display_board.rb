449a65ee58c643442dcb0cb03ad99d938ec3ffa7
Andrew Silva <andrewsilva9@gmail.com>
2019-03-26 23:32:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
-board = [" ", " ", " ", " 4 ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " X ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
